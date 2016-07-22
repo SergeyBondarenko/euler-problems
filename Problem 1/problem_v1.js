ce5878f19b2b025b53baef942abbeab05d2fa017
SergeyBondarenko <sergibondarenko@gmail.com>
2016-07-22 17:48:29 +0200

    Add pr 1 solution in JS.

--- Problem 1/problem_v1.js
+++ Problem 1/problem_v1.js
@@ -5,4 +5,11 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
+var i, n = 0;
+for (i = 0; i < 1000; i++) {
+  if (i % 3 == 0 || i % 5 == 0) {
+    n += i;
+  }
+}
 
+console.log(n);
